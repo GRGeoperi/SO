77197c30468345345450589a2cda81e233388139
Geoperi González Roldán <ggonzalezr2100@alumno.ipn.mx>
2024-03-06 13:23:22 -0600

    Agregado el uso de if

--- primer_parcial/script.sh
+++ primer_parcial/script.sh
@@ -3,3 +3,15 @@
 # Crear un script que contenga las siguientes estructuras de control:
 # IF, FOR, WHILE, CASE.
 # Incluír variables con asignación de diferentes tipos de datos
+
+echo "Bienvenido a la calculadora escrita en shell"
+if test "$3" = "+"; then
+    SUMA=$(($1+$2))
+    echo "Su suma es: $SUMA"
+elif test "$3" = "-"; then
+    RESTA=$(($1-$2))
+    echo "Su resta es: $RESTA"
+elif test "$3" = "/"; then
+    DIVISION=$(($1/$2))
+    echo "Su división es: $DIVISION"
+fi
