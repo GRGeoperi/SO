d72f0f4fd411f3061e25f5dd638f7e39b65cbce6
Geoperi González Roldán <ggonzalezr2100@alumno.ipn.mx>
2024-03-07 12:22:06 -0600

    Agradas las instrucciones del funcionamiento del programa

---
+++ primer_parcial/calculadora.sh
@@ -0,0 +1,8 @@
+# Crear un shellscript que realice las operaciones básicas de
+# una calculadora, tomando en cuenta lo siguiente:
+# 1. Validar que el usuario introduzca al menos un argumento al
+# momento de ejecutar el script.
+# 2. El argumento indica la operación que se desea realizar,
+# ejemplo ./calculadora.sh suma.
+# 3. Usar un CASE y funciones para realizar las operaciones
+# necesarias.
