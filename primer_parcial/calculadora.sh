3d5adf68f93f8459da114932e9d89653f9ec025f
Geoperi González Roldán <ggonzalezr2100@alumno.ipn.mx>
2024-03-07 12:55:52 -0600

    Agregadas las funciones básicas de la calculadora

--- primer_parcial/calculadora.sh
+++ primer_parcial/calculadora.sh
@@ -6,3 +6,55 @@
 # ejemplo ./calculadora.sh suma.
 # 3. Usar un CASE y funciones para realizar las operaciones
 # necesarias.
+
+function sumar()
+{
+    read -p "Introduce el primer número: " VAR1
+    read -p "Introduce el segundo número: " VAR2
+    return $(($VAR1+$VAR2))
+}
+
+function restar()
+{
+    read -p "Introduce el primer número: " VAR1
+    read -p "Introduce el segundo número: " VAR2
+    return $(($VAR1-$VAR2))
+}
+
+function multiplicar()
+{
+    read -p "Introduce el primer número: " VAR1
+    read -p "Introduce el segundo número: " VAR2
+    return $(($VAR1*$VAR2))
+}
+
+function dividir()
+{
+    read -p "Introduce el primer número: " VAR1
+    read -p "Introduce el segundo número: " VAR2
+    return $(($VAR1/$VAR2))
+}
+
+if test "$#" != 0; then
+    echo "¡Bienvenido a la calculadora escrita en shell!"
+    case $1 in
+        suma)
+            sumar
+            echo "Su suma es: $?"
+            ;;
+        resta)
+            restar
+            echo "Su resta es: $?"
+            ;;
+        multiplicacion)
+            multiplicar
+            echo "Su multiplicación es: $?"
+            ;;
+        division)
+            dividir
+            echo "Su división es: $?"
+            ;;
+    esac
+else
+    echo "Argumentos faltantes"
+fi
