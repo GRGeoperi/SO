fc90bdf31d9621de6145aed7841bb45ccae99dfa
Geoperi González Roldán <ggonzalezr2100@alumno.ipn.mx>
2024-05-09 14:53:20 -0600

    Arreglada la impresión en pantalla de forma correcta

--- cuarto_bloque/tablas.c
+++ cuarto_bloque/tablas.c
@@ -1,24 +1,54 @@
 #include "ftablas.c"
+#include "fsemaforo.c"
 
 int main(void)
 {
     pid_t pid;
-    for (int i = 2; i <= 10; i++)
+    int tareasRealizadas = 0;
+    int idGrupoSemaforos = crear_semaforos(10);
+
+    // Iniciar al semáforo del padre en 1
+    inicializar_semaforos_1(idGrupoSemaforos, 0);
+    // Iniciar a los demás semaforos en 0
+    for (int i = 1; i <= 9; i++)
+    {
+        inicializar_semaforos_0(idGrupoSemaforos, i);
+    }
+
+    for (int i = 1; i <= 9; i++)
     {
         pid = try_fork();
-        // Procesos hijo
         if (pid == 0)
         {
-            print_table(i);
+            // Decrementar el valor del semáforo del hijo correspondiente
+            operacion_p(idGrupoSemaforos, i);
+            // Imprimir la tabla correspondiente
+            print_table(i+1);
+            // Actualizar la cantidad de tareas realizadas
+            tareasRealizadas++;
+            // Incrementar el valor del semáforo del hijo siguiente
+            if (i < 9)
+            {
+                operacion_v(idGrupoSemaforos, i+1);
+            }
             return 0;
         }
     }
-    // Proceso padre
+    // Decrementar el valor del semáforo del padre
+    operacion_p(idGrupoSemaforos, 0);
+    // Imprimir la tabla correspondiente
     print_table(1);
-    // Esperar a que todos sus hijos terminen
-    for (int i = 0; i < 9; i++)
+    // Actualizar la cantidad de tareas realizadas
+    tareasRealizadas++;
+    // Incrementar el valor del semáforo del primer hijo
+    operacion_v(idGrupoSemaforos, 1);
+    // Destruir todos los semáforos
+    if (tareasRealizadas == 10)
     {
-        wait(NULL);
+        for (int i = 0; i < 10; i++)
+        {
+            destruir_semaforo(idGrupoSemaforos, i);
+        }
     }
     return 0;
 }
